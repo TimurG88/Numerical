141fca96ba56083939c71c7850bff1f62ba7ce93
TimurG88 <91155172+TimurG88@users.noreply.github.com>
2022-01-21 15:41:52 +0300

    Add files via upload

---
+++ src/Help/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+
+namespace Help
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [DebuggerNonUserCode]
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
